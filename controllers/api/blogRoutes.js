420300fba55bde7f56adde3a10087f67fd981d14
KNC2007 <cnkalbach@gmail.com>
2024-01-24 22:15:32 -0500

    fixing errors in creating new blog

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,14 +1,10 @@
 const router = require('express').Router();
 const { Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
-// // helps read maps_api_key
 // require('dotenv').config();
 
 router.post('/', withAuth, async (req, res) => {
-  // // reading maps_api_key
-  // if(process.env.MAPS_API_KEY){
-  //   console.log("I have the api key for maps");
-  // }
+
   try {
     const newBlog = await Blog.create({
       ...req.body,
